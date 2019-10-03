7eedbc1768e2d7dfaefcbf2386a72b98b7fa7c64
Jonathan Karon <jkaron@newrelic.com>
2019-10-03 13:20:42 -0700

    add fuzzy matching for detected agent version vs. docs content because consistency is hard

--- nerdlets/groundskeeper-nerdlet/index.js
+++ nerdlets/groundskeeper-nerdlet/index.js
@@ -597,8 +597,30 @@ export default class Groundskeeper extends React.Component {
 
 function agentAge(a, agentVersions) {
   const langVersions = agentVersions[a.language];
+  const reportedVersion = a.agentVersions[0];
+  if (!reportedVersion) {
+    return undefined;
+  }
+
+  // sometimes agents include build numbers but the docs don't
+  // so we'll be tolerant of matching on the first 3 segments of a semver
+  const mNumberedVersion = reportedVersion.match(/(\d+\.\d+\.\d+)\.\d+$/);
+  const numberedVersion =
+    mNumberedVersion && mNumberedVersion[1] ? mNumberedVersion[1] : undefined;
+
+  // sometimes agent versions in the docs API don't include a trailing `.0`
+  // so we'll be tolerant of a match after stripping `.0` suffix from the shortest variant of the reported version
+  const mShortVersion = (numberedVersion || reportedVersion).match(/(.+)\.0$/);
+  const shortVersion =
+    mShortVersion && mShortVersion[1] ? mShortVersion[1] : undefined;
+
   const agentVersion = langVersions
-    ? langVersions.find(v => v.version === a.agentVersions[0])
+    ? langVersions.find(
+        v =>
+          v.version === reportedVersion ||
+          v.version === numberedVersion ||
+          v.version === shortVersion
+      )
     : '';
   return agentVersion ? agentVersion.date : undefined;
 }
@@ -606,9 +628,10 @@ function agentAge(a, agentVersions) {
 /**
  * Some agent releases have a `v` prefix on the version number in the New Relic docs.
  * We use this filter to strip those out so the version string matches what's reported by the agent itself.
+ * Filter also strips build numbers from semver strings because our docs use them inconsistently.
  */
 function cleanAgentVersion(version) {
-  const m = (version || '').match(/(\d+.*)/);
+  const m = (version || '').match(/(\d+\.\d+\.\d+)/);
   if (m && m[1]) {
     return m[1];
   }
